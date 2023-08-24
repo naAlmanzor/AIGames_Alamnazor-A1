84d53a08d58bcd71ab85ddd907ded75f2bf61852
Neil Angelo Almaznor <60123816+naAlmanzor@users.noreply.github.com>
2023-08-24 23:55:18 +0800

    Updated Rocket AI
    - Separated raycast to top and bottom
    
    - Added movement when raycast detects incoming asteroid

--- Ships/Assets/Scripts/RocketScript.cs
+++ Ships/Assets/Scripts/RocketScript.cs
@@ -6,29 +6,53 @@ public class RocketScript : MonoBehaviour
 {
     public float speed = 5;
     private Rigidbody2D rb; 
-    public GameObject obstacleRaycast;
+    public GameObject topObstacleRaycast;
+    public GameObject bottomObstacleRaycast;
     public float raycastDistance;
+    private float GetVerticalSpeed() => rb.velocity.y;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(speed, 0);
+        rb.velocity = new Vector2(speed, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hitObstacle = Physics2D.Raycast(obstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance);
+        RaycastHit2D topHit = Physics2D.Raycast(topObstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance);
+        RaycastHit2D bottomHit = Physics2D.Raycast(bottomObstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance);
+        
+        if(topHit){
+            Debug.Log(topHit.collider.name);
+            Debug.DrawRay(topObstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance, Color.red);
+            
+            if (GetVerticalSpeed() == 0){
+                rb.velocity = new Vector2(speed*5, -10f).normalized;
+            }
+        }
 
-        if(hitObstacle){
-            Debug.Log(hitObstacle.collider.name);
-            Debug.DrawRay(obstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance, Color.red);
+        else if(bottomHit){
+            Debug.Log(bottomHit.collider.name);
+            Debug.DrawRay(bottomObstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance, Color.red);
+
+            if (GetVerticalSpeed() == 0){
+                rb.velocity = new Vector2(speed*5, -10f).normalized;
+            }
         }
 
         else{
-            Debug.Log("All Clear!");
-            Debug.DrawRay(obstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance, Color.blue);
+            if(!topHit){
+                Debug.Log("All Clear Up Above!");
+                Debug.DrawRay(topObstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance, Color.blue);
+            }
+            if(!bottomHit){
+                Debug.Log("All Clear Up Below!");
+                Debug.DrawRay(bottomObstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * raycastDistance, Color.blue);
+            }
+
+            rb.velocity = new Vector2(speed, 0f);
         }
     }
     

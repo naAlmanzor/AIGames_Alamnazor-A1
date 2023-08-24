6beb746d56aa8775f643c18003533c2b36437ea8
Neil Angelo Almaznor <60123816+naAlmanzor@users.noreply.github.com>
2023-08-24 12:03:32 +0800

    Fixed raycast issue
    - Fixed issue where Raycast does not detect asteroid

--- Ships/Assets/Scripts/RocketScript.cs
+++ Ships/Assets/Scripts/RocketScript.cs
@@ -6,6 +6,8 @@ public class RocketScript : MonoBehaviour
 {
     public float speed = 5;
     private Rigidbody2D rb; 
+    public GameObject obstacleRaycast;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +18,21 @@ public class RocketScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D hitObstacle = Physics2D.Raycast(obstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * 10f);
+
+        if(hitObstacle){
+            Debug.Log(hitObstacle.collider.name);
+            Debug.DrawRay(obstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * 10f, Color.red);
+        }
+
+        else{
+            Debug.Log("All Clear!");
+            Debug.DrawRay(obstacleRaycast.transform.position, transform.TransformDirection(Vector2.right) * 10f, Color.red);
+        }
     }
     
-    private void OnTriggerEnter2D(Collider2D other){
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag == "Asteroid")
         Debug.Log("hit");
         Destroy(other.gameObject);
         this.gameObject.SetActive(false);
